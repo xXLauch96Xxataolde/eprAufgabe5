ff4a7fafd24b136ee17e1b20495f6af91676bc5e
Robert <Robert@172.22.84.215>
2017-12-08 13:58:15 +0100

    gird made

--- scr/pah_tum/pah_tum_controller.py
+++ scr/pah_tum/pah_tum_controller.py
@@ -4,6 +4,7 @@ Additional information if required and more infos. Complete sentences please.
 """
 
 import random  
+import tkinter as tk
 
 __author__ = "123456: John Cleese, 654321: Terry Gilliam"  # put your data here
 __copyright__ = "Copyright 2017/2018 - EPR-Goethe-Uni" 
@@ -11,5 +12,22 @@ __credits__ = "If you would like to thank somebody \
               i.e. an other student for her code or leave it out" 
 __email__ = "your email address" 
 
+
+def color_change(event):
+    print("something", event.widget)   
+    event.widget.config(bg="lightgreen")
+
+
 def controller():
+    root = tk.Tk()
+    for i in range(7):
+        for j in range(7):
+            a = "l"+str(i+1)+str(j+1)
+            a = tk.Label(root, text=a, width = 5, height = 5)
+            a.bind("<Button-1>", color_change)
+            a.grid(row=i, column = j)
+        
     print("do some shit")
+    33
+    
+    root.mainloop()
