2ab3f308fdbfc3ec7ca14724812070af9a05e175
Robert <Robert@172.22.84.215>
2017-12-08 13:28:59 +0100

    first step

---
+++ scr/pah_tum/pah_tum_controller.py
@@ -0,0 +1,15 @@
+"""Docstring: A very short sentence explaining the function. < 79 characters. 
+
+Additional information if required and more infos. Complete sentences please.
+"""
+
+import random  
+
+__author__ = "123456: John Cleese, 654321: Terry Gilliam"  # put your data here
+__copyright__ = "Copyright 2017/2018 - EPR-Goethe-Uni" 
+__credits__ = "If you would like to thank somebody \
+              i.e. an other student for her code or leave it out" 
+__email__ = "your email address" 
+
+def pah_tum_controller():
+    print("do some shit")
